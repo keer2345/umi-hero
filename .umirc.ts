89f15fb5b4ea355bff43d0b08b4b8e1d373c1642
keer2345 <keer2345@gmail.com>
2022-03-30 12:49:49 +0800

    prettier of semi is false

--- .umirc.ts
+++ .umirc.ts
@@ -1,11 +1,9 @@
-import { defineConfig } from 'umi';
+import { defineConfig } from 'umi'
 
 export default defineConfig({
   nodeModulesTransform: {
     type: 'none',
   },
-  routes: [
-    { path: '/', component: '@/pages/index' },
-  ],
+  routes: [{ path: '/', component: '@/pages/index' }],
   fastRefresh: {},
-});
+})
