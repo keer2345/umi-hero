d6d649e234b3538ceba2cff25a488cd6b4cd4c62
keer2345 <keer2345@gmail.com>
2022-03-31 22:07:59 +0800

    监听路由事件

--- src/pages/hero/index.tsx
+++ src/pages/hero/index.tsx
@@ -11,6 +11,7 @@ const Hero: FC<PagePros> = (props) => {
     <div>
       <h1>Page hero</h1>
       <h2>This is {props.hero.name}</h2>
+      <h3>This is {JSON.stringify(props.hero)}</h3>
     </div>
   )
 }
--- src/pages/hero/models/hero.ts
+++ src/pages/hero/models/hero.ts
@@ -2,18 +2,20 @@ import { Effect, ImmerReducer, Reducer, Subscription } from 'umi'
 
 export interface HeroModelState {
   name: string
+  heros: []
 }
 
 export interface HeroModelType {
   namespace: 'hero'
   state: HeroModelState
   effects: {
-    query: Effect
+    // query: Effect
+    fetch: Effect
   }
   reducers: {
-    // save: Reducer<HeroModelState>
+    save: Reducer<HeroModelState>
     // 启用 immer 之后
-    save: ImmerReducer<HeroModelState>
+    // save: ImmerReducer<HeroModelState>
   }
   subscriptions: { setup: Subscription }
 }
@@ -22,32 +24,54 @@ const HeroModel: HeroModelType = {
   namespace: 'hero',
 
   state: {
-    name: 'hero',
+    name: '',
+    heros: [],
   },
 
   effects: {
-    *query({ payload }, { call, put }) {},
+    // *query({ payload }, { call, put }) {},
+    *fetch({ type, payload }, { call, put, select }) {
+      const data = [
+        {
+          ename: 105,
+          cname: '廉颇',
+          title: '正义爆轰',
+          new_type: 0,
+          hero_type: 3,
+          skin_name: '正义爆轰|地狱岩魂',
+        },
+        {
+          ename: 106,
+          cname: '小乔',
+          title: '恋之微风',
+          new_type: 0,
+          hero_type: 2,
+          skin_name: '恋之微风|万圣前夜|天鹅之梦|纯白花嫁|缤纷独角兽',
+        },
+      ]
+      yield put({ type: 'save', payload: { heros: data } })
+    },
   },
 
   reducers: {
-    // save(state, action) {
-    //   return {
-    //     ...state,
-    //     ...action.payload,
-    //   }
-    // },
-    // 启用 immer 之后
     save(state, action) {
-      state.name = action.payload
+      return {
+        ...state,
+        ...action.payload,
+      }
     },
+    // 启用 immer 之后
+    // save(state, action) {
+    //   state.name = action.payload
+    // },
   },
 
   subscriptions: {
     setup({ dispatch, history }) {
       return history.listen(({ pathname }) => {
-        if (pathname === '/') {
+        if (pathname === '/hero') {
           dispatch({
-            type: 'query',
+            type: 'fetch',
           })
         }
       })
